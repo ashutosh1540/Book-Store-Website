1e9cf03771702cb1cf94155e400e48463dbbbc61
Ashutosh Kumar <105875328+ashutosh1540@users.noreply.github.com>
2023-09-06 22:08:05 +0530

    orders.php

--- orders.php
+++ orders.php
@@ -1,17 +1,11 @@
 <?php
-
 include 'config.php';
-
 session_start();
-
 $user_id = $_SESSION['user_id'];
-
 if(!isset($user_id)){
    header('location:login.php');
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,29 +13,18 @@ if(!isset($user_id)){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>orders</title>
-
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
-   
 <?php include 'header.php'; ?>
-
 <div class="heading">
    <h3>your orders</h3>
    <p> <a href="home.php">home</a> / orders </p>
 </div>
-
 <section class="placed-orders">
-
    <h1 class="title">placed orders</h1>
-
    <div class="box-container">
-
       <?php
          $order_query = mysqli_query($conn, "SELECT * FROM `orders` WHERE user_id = '$user_id'") or die('query failed');
          if(mysqli_num_rows($order_query) > 0){
@@ -65,20 +48,8 @@ if(!isset($user_id)){
       }
       ?>
    </div>
-
 </section>
-
-
-
-
-
-
-
-
 <?php include 'footer.php'; ?>
-
-<!-- custom js file link  -->
 <script src="js/script.js"></script>
-
 </body>
 </html>

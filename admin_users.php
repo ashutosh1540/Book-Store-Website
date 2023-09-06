44e95eea91e511014f4af892e673e950f2b66cf1
Ashutosh Kumar <105875328+ashutosh1540@users.noreply.github.com>
2023-09-06 20:45:01 +0530

    admin_users.php

--- admin_users.php
+++ admin_users.php
@@ -1,23 +1,16 @@
 <?php
-
 include 'config.php';
-
 session_start();
-
 $admin_id = $_SESSION['admin_id'];
-
 if(!isset($admin_id)){
    header('location:login.php');
 }
-
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
    mysqli_query($conn, "DELETE FROM `users` WHERE id = '$delete_id'") or die('query failed');
    header('location:admin_users.php');
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,22 +18,13 @@ if(isset($_GET['delete'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>users</title>
-
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-   <!-- custom admin css file link  -->
    <link rel="stylesheet" href="css/admin_style.css">
-
 </head>
 <body>
-   
 <?php include 'admin_header.php'; ?>
-
 <section class="users">
-
    <h1 class="title"> user accounts </h1>
-
    <div class="box-container">
       <?php
          $select_users = mysqli_query($conn, "SELECT * FROM `users`") or die('query failed');
@@ -57,19 +41,7 @@ if(isset($_GET['delete'])){
          };
       ?>
    </div>
-
 </section>
-
-
-
-
-
-
-
-
-
-<!-- custom admin js file link  -->
 <script src="js/admin_script.js"></script>
-
 </body>
 </html>

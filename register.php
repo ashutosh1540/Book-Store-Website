8fd55094004ff68d5f57b6b3ff1d6d835cf91868
Ashutosh Kumar <105875328+ashutosh1540@users.noreply.github.com>
2023-09-06 22:10:40 +0530

    register.php

--- register.php
+++ register.php
@@ -1,17 +1,12 @@
 <?php
-
 include 'config.php';
-
 if(isset($_POST['submit'])){
-
    $name = mysqli_real_escape_string($conn, $_POST['name']);
    $email = mysqli_real_escape_string($conn, $_POST['email']);
    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
    $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
    $user_type = $_POST['user_type'];
-
    $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
-
    if(mysqli_num_rows($select_users) > 0){
       $message[] = 'user already exist!';
    }else{
@@ -23,11 +18,8 @@ if(isset($_POST['submit'])){
          header('location:login.php');
       }
    }
-
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,18 +27,10 @@ if(isset($_POST['submit'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>register</title>
-
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
-
-
-
 <?php
 if(isset($message)){
    foreach($message as $message){
@@ -59,9 +43,7 @@ if(isset($message)){
    }
 }
 ?>
-   
 <div class="form-container">
-
    <form action="" method="post">
       <h3>register now</h3>
       <input type="text" name="name" placeholder="enter your name" required class="box">
@@ -75,8 +57,6 @@ if(isset($message)){
       <input type="submit" name="submit" value="register now" class="btn">
       <p>already have an account? <a href="login.php">login now</a></p>
    </form>
-
 </div>
-
 </body>
 </html>

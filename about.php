7be90a6ed8e455738aa3bbcc8119ead3ac14fadd
Ashutosh Kumar <105875328+ashutosh1540@users.noreply.github.com>
2023-09-06 20:28:04 +0530

    about.php

--- about.php
+++ about.php
@@ -1,17 +1,11 @@
 <?php
-
 include 'config.php';
-
 session_start();
-
 $user_id = $_SESSION['user_id'];
-
 if(!isset($user_id)){
    header('location:login.php');
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,48 +13,32 @@ if(!isset($user_id)){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>about</title>
-
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
-   
 <?php include 'header.php'; ?>
-
 <div class="heading">
    <h3>about us</h3>
    <p> <a href="home.php">home</a> / about </p>
 </div>
-
 <section class="about">
-
    <div class="flex">
-
       <div class="image">
          <img src="images/about-img.jpg" alt="">
       </div>
-
       <div class="content">
          <h3>why choose us?</h3>
          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet voluptatibus aut hic molestias, reiciendis natus fuga, cumque excepturi veniam ratione iure. Excepturi fugiat placeat iusto facere id officia assumenda temporibus?</p>
          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quos enim minima ipsa dicta officia corporis ratione saepe sed adipisci?</p>
          <a href="contact.php" class="btn">contact us</a>
       </div>
-
    </div>
-
 </section>
-
 <section class="reviews">
-
    <h1 class="title">client's reviews</h1>
-
    <div class="box-container">
-
       <div class="box">
          <img src="images/pic-1.png" alt="">
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
@@ -73,7 +51,6 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
       <div class="box">
          <img src="images/pic-2.png" alt="">
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
@@ -86,7 +63,6 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
       <div class="box">
          <img src="images/pic-3.png" alt="">
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
@@ -99,7 +75,6 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
       <div class="box">
          <img src="images/pic-4.png" alt="">
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
@@ -112,7 +87,6 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
       <div class="box">
          <img src="images/pic-5.png" alt="">
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
@@ -125,7 +99,6 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
       <div class="box">
          <img src="images/pic-6.png" alt="">
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
@@ -138,17 +111,11 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
    </div>
-
 </section>
-
 <section class="authors">
-
    <h1 class="title">greate authors</h1>
-
    <div class="box-container">
-
       <div class="box">
          <img src="images/author-1.jpg" alt="">
          <div class="share">
@@ -159,7 +126,6 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
       <div class="box">
          <img src="images/author-2.jpg" alt="">
          <div class="share">
@@ -170,7 +136,6 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
       <div class="box">
          <img src="images/author-3.jpg" alt="">
          <div class="share">
@@ -181,7 +146,6 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
       <div class="box">
          <img src="images/author-4.jpg" alt="">
          <div class="share">
@@ -192,7 +156,6 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
       <div class="box">
          <img src="images/author-5.jpg" alt="">
          <div class="share">
@@ -203,7 +166,6 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
       <div class="box">
          <img src="images/author-6.jpg" alt="">
          <div class="share">
@@ -214,21 +176,9 @@ if(!isset($user_id)){
          </div>
          <h3>john deo</h3>
       </div>
-
    </div>
-
 </section>
-
-
-
-
-
-
-
 <?php include 'footer.php'; ?>
-
-<!-- custom js file link  -->
 <script src="js/script.js"></script>
-
 </body>
 </html>

c764171707947934f15f0659c6f56bc35fbb16e5
Ashutosh Kumar <105875328+ashutosh1540@users.noreply.github.com>
2023-09-06 21:58:35 +0530

    footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,5 @@
 <section class="footer">
-
    <div class="box-container">
-
       <div class="box">
          <h3>quick links</h3>
          <a href="home.php">home</a>
@@ -9,7 +7,6 @@
          <a href="shop.php">shop</a>
          <a href="contact.php">contact</a>
       </div>
-
       <div class="box">
          <h3>extra links</h3>
          <a href="login.php">login</a>
@@ -17,7 +14,6 @@
          <a href="cart.php">cart</a>
          <a href="orders.php">orders</a>
       </div>
-
       <div class="box">
          <h3>contact info</h3>
          <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
@@ -25,7 +21,6 @@
          <p> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </p>
          <p> <i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </p>
       </div>
-
       <div class="box">
          <h3>follow us</h3>
          <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
@@ -33,9 +28,6 @@
          <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
          <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
       </div>
-
    </div>
-
    <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>mr. web designer</span> </p>
-
 </section>

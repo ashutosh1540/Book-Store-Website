1022ff69768473d060892c5b2481673e7a25f591
Ashutosh Kumar <105875328+ashutosh1540@users.noreply.github.com>
2023-09-06 20:38:57 +0530

    admin_page.php

--- admin_page.php
+++ admin_page.php
@@ -1,17 +1,11 @@
 <?php
-
 include 'config.php';
-
 session_start();
-
 $admin_id = $_SESSION['admin_id'];
-
 if(!isset($admin_id)){
    header('location:login.php');
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,26 +13,14 @@ if(!isset($admin_id)){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>admin panel</title>
-
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-   <!-- custom admin css file link  -->
    <link rel="stylesheet" href="css/admin_style.css">
-
 </head>
 <body>
-   
 <?php include 'admin_header.php'; ?>
-
-<!-- admin dashboard section starts  -->
-
 <section class="dashboard">
-
    <h1 class="title">dashboard</h1>
-
    <div class="box-container">
-
       <div class="box">
          <?php
             $total_pendings = 0;
@@ -53,7 +35,6 @@ if(!isset($admin_id)){
          <h3>$<?php echo $total_pendings; ?>/-</h3>
          <p>total pendings</p>
       </div>
-
       <div class="box">
          <?php
             $total_completed = 0;
@@ -68,7 +49,6 @@ if(!isset($admin_id)){
          <h3>$<?php echo $total_completed; ?>/-</h3>
          <p>completed payments</p>
       </div>
-
       <div class="box">
          <?php 
             $select_orders = mysqli_query($conn, "SELECT * FROM `orders`") or die('query failed');
@@ -77,7 +57,6 @@ if(!isset($admin_id)){
          <h3><?php echo $number_of_orders; ?></h3>
          <p>order placed</p>
       </div>
-
       <div class="box">
          <?php 
             $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
@@ -86,7 +65,6 @@ if(!isset($admin_id)){
          <h3><?php echo $number_of_products; ?></h3>
          <p>products added</p>
       </div>
-
       <div class="box">
          <?php 
             $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'user'") or die('query failed');
@@ -95,7 +73,6 @@ if(!isset($admin_id)){
          <h3><?php echo $number_of_users; ?></h3>
          <p>normal users</p>
       </div>
-
       <div class="box">
          <?php 
             $select_admins = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'admin'") or die('query failed');
@@ -104,7 +81,6 @@ if(!isset($admin_id)){
          <h3><?php echo $number_of_admins; ?></h3>
          <p>admin users</p>
       </div>
-
       <div class="box">
          <?php 
             $select_account = mysqli_query($conn, "SELECT * FROM `users`") or die('query failed');
@@ -113,7 +89,6 @@ if(!isset($admin_id)){
          <h3><?php echo $number_of_account; ?></h3>
          <p>total accounts</p>
       </div>
-
       <div class="box">
          <?php 
             $select_messages = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
@@ -122,23 +97,8 @@ if(!isset($admin_id)){
          <h3><?php echo $number_of_messages; ?></h3>
          <p>new messages</p>
       </div>
-
    </div>
-
 </section>
-
-<!-- admin dashboard section ends -->
-
-
-
-
-
-
-
-
-
-<!-- custom admin js file link  -->
 <script src="js/admin_script.js"></script>
-
 </body>
 </html>

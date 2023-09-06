de15ec86d270777fe371d0b3e2923b3482515101
Ashutosh Kumar <105875328+ashutosh1540@users.noreply.github.com>
2023-09-06 20:36:51 +0530

    admin_orders.php

--- admin_orders.php
+++ admin_orders.php
@@ -1,32 +1,22 @@
 <?php
-
 include 'config.php';
-
 session_start();
-
 $admin_id = $_SESSION['admin_id'];
-
 if(!isset($admin_id)){
    header('location:login.php');
 }
-
 if(isset($_POST['update_order'])){
-
    $order_update_id = $_POST['order_id'];
    $update_payment = $_POST['update_payment'];
    mysqli_query($conn, "UPDATE `orders` SET payment_status = '$update_payment' WHERE id = '$order_update_id'") or die('query failed');
    $message[] = 'payment status has been updated!';
-
 }
-
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
    mysqli_query($conn, "DELETE FROM `orders` WHERE id = '$delete_id'") or die('query failed');
    header('location:admin_orders.php');
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -34,22 +24,13 @@ if(isset($_GET['delete'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>orders</title>
-
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-   <!-- custom admin css file link  -->
    <link rel="stylesheet" href="css/admin_style.css">
-
 </head>
 <body>
-   
 <?php include 'admin_header.php'; ?>
-
 <section class="orders">
-
    <h1 class="title">placed orders</h1>
-
    <div class="box-container">
       <?php
       $select_orders = mysqli_query($conn, "SELECT * FROM `orders`") or die('query failed');
@@ -84,20 +65,7 @@ if(isset($_GET['delete'])){
       }
       ?>
    </div>
-
 </section>
-
-
-
-
-
-
-
-
-
-
-<!-- custom admin js file link  -->
 <script src="js/admin_script.js"></script>
-
 </body>
 </html>

02f9740f1d5313e191b1ca25409886c9ad36e9be
Ashutosh Kumar <105875328+ashutosh1540@users.noreply.github.com>
2023-09-06 20:47:27 +0530

    cart.php

--- cart.php
+++ cart.php
@@ -1,35 +1,26 @@
 <?php
-
 include 'config.php';
-
 session_start();
-
 $user_id = $_SESSION['user_id'];
-
 if(!isset($user_id)){
    header('location:login.php');
 }
-
 if(isset($_POST['update_cart'])){
    $cart_id = $_POST['cart_id'];
    $cart_quantity = $_POST['cart_quantity'];
    mysqli_query($conn, "UPDATE `cart` SET quantity = '$cart_quantity' WHERE id = '$cart_id'") or die('query failed');
    $message[] = 'cart quantity updated!';
 }
-
 if(isset($_GET['delete'])){
    $delete_id = $_GET['delete'];
    mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$delete_id'") or die('query failed');
    header('location:cart.php');
 }
-
 if(isset($_GET['delete_all'])){
    mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
    header('location:cart.php');
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,27 +28,17 @@ if(isset($_GET['delete_all'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>cart</title>
-
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
-   
 <?php include 'header.php'; ?>
-
 <div class="heading">
    <h3>shopping cart</h3>
    <p> <a href="home.php">home</a> / cart </p>
 </div>
-
 <section class="shopping-cart">
-
    <h1 class="title">products added</h1>
-
    <div class="box-container">
       <?php
          $grand_total = 0;
@@ -85,11 +66,9 @@ if(isset($_GET['delete_all'])){
       }
       ?>
    </div>
-
    <div style="margin-top: 2rem; text-align:center;">
       <a href="cart.php?delete_all" class="delete-btn <?php echo ($grand_total > 1)?'':'disabled'; ?>" onclick="return confirm('delete all from cart?');">delete all</a>
    </div>
-
    <div class="cart-total">
       <p>grand total : <span>$<?php echo $grand_total; ?>/-</span></p>
       <div class="flex">
@@ -97,20 +76,8 @@ if(isset($_GET['delete_all'])){
          <a href="checkout.php" class="btn <?php echo ($grand_total > 1)?'':'disabled'; ?>">proceed to checkout</a>
       </div>
    </div>
-
 </section>
-
-
-
-
-
-
-
-
 <?php include 'footer.php'; ?>
-
-<!-- custom js file link  -->
 <script src="js/script.js"></script>
-
 </body>
 </html>

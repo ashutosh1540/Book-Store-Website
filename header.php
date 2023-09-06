17563cd728485e3cffa155ed94310bd9773b44cf
Ashutosh Kumar <105875328+ashutosh1540@users.noreply.github.com>
2023-09-06 21:59:36 +0530

    header.php

--- header.php
+++ header.php
@@ -10,9 +10,7 @@ if(isset($message)){
    }
 }
 ?>
-
 <header class="header">
-
    <div class="header-1">
       <div class="flex">
          <div class="share">
@@ -24,11 +22,9 @@ if(isset($message)){
          <p> new <a href="login.php">login</a> | <a href="register.php">register</a> </p>
       </div>
    </div>
-
    <div class="header-2">
       <div class="flex">
          <a href="home.php" class="logo">Bookly.</a>
-
          <nav class="navbar">
             <a href="home.php">home</a>
             <a href="about.php">about</a>
@@ -36,7 +32,6 @@ if(isset($message)){
             <a href="contact.php">contact</a>
             <a href="orders.php">orders</a>
          </nav>
-
          <div class="icons">
             <div id="menu-btn" class="fas fa-bars"></div>
             <a href="search_page.php" class="fas fa-search"></a>
@@ -47,7 +42,6 @@ if(isset($message)){
             ?>
             <a href="cart.php"> <i class="fas fa-shopping-cart"></i> <span>(<?php echo $cart_rows_number; ?>)</span> </a>
          </div>
-
          <div class="user-box">
             <p>username : <span><?php echo $_SESSION['user_name']; ?></span></p>
             <p>email : <span><?php echo $_SESSION['user_email']; ?></span></p>
@@ -55,5 +49,4 @@ if(isset($message)){
          </div>
       </div>
    </div>
-
 </header>

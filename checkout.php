5e4328b6c65fc133d6390cb5e537dfbbf5d15546
Ashutosh Kumar <105875328+ashutosh1540@users.noreply.github.com>
2023-09-06 20:54:55 +0530

    checkout.php

--- checkout.php
+++ checkout.php
@@ -1,27 +1,19 @@
 <?php
-
 include 'config.php';
-
 session_start();
-
 $user_id = $_SESSION['user_id'];
-
 if(!isset($user_id)){
    header('location:login.php');
 }
-
 if(isset($_POST['order_btn'])){
-
    $name = mysqli_real_escape_string($conn, $_POST['name']);
    $number = $_POST['number'];
    $email = mysqli_real_escape_string($conn, $_POST['email']);
    $method = mysqli_real_escape_string($conn, $_POST['method']);
    $address = mysqli_real_escape_string($conn, 'flat no. '. $_POST['flat'].', '. $_POST['street'].', '. $_POST['city'].', '. $_POST['country'].' - '. $_POST['pin_code']);
    $placed_on = date('d-M-Y');
-
    $cart_total = 0;
    $cart_products[] = '';
-
    $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
    if(mysqli_num_rows($cart_query) > 0){
       while($cart_item = mysqli_fetch_assoc($cart_query)){
@@ -30,11 +22,8 @@ if(isset($_POST['order_btn'])){
          $cart_total += $sub_total;
       }
    }
-
    $total_products = implode(', ',$cart_products);
-
    $order_query = mysqli_query($conn, "SELECT * FROM `orders` WHERE name = '$name' AND number = '$number' AND email = '$email' AND method = '$method' AND address = '$address' AND total_products = '$total_products' AND total_price = '$cart_total'") or die('query failed');
-
    if($cart_total == 0){
       $message[] = 'your cart is empty';
    }else{
@@ -46,11 +35,8 @@ if(isset($_POST['order_btn'])){
          mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
       }
    }
-   
 }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -58,25 +44,16 @@ if(isset($_POST['order_btn'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>checkout</title>
-
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
-   
 <?php include 'header.php'; ?>
-
 <div class="heading">
    <h3>checkout</h3>
    <p> <a href="home.php">home</a> / checkout </p>
 </div>
-
 <section class="display-order">
-
    <?php  
       $grand_total = 0;
       $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
@@ -93,11 +70,8 @@ if(isset($_POST['order_btn'])){
    }
    ?>
    <div class="grand-total"> grand total : <span>$<?php echo $grand_total; ?>/-</span> </div>
-
 </section>
-
 <section class="checkout">
-
    <form action="" method="post">
       <h3>place your order</h3>
       <div class="flex">
@@ -149,21 +123,8 @@ if(isset($_POST['order_btn'])){
       </div>
       <input type="submit" value="order now" class="btn" name="order_btn">
    </form>
-
 </section>
-
-
-
-
-
-
-
-
-
 <?php include 'footer.php'; ?>
-
-<!-- custom js file link  -->
 <script src="js/script.js"></script>
-
 </body>
 </html>
